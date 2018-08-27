5c25391c3b7b615ec8912c54f1817b06c95677ec
lip <lip@hojy.com>
2018-08-27 15:05:49 +0800

    获取页面大小调整图像大小

--- js/myecharts.js
+++ js/myecharts.js
@@ -1,237 +1,215 @@
-var docChart = document.getElementById("main")
-var myChart = echarts.init(docChart);
 
-function DrawWeight(yData, xData, xAxisName, yAxisName) {
-    var option = {
-        baseOption: {
-            xAxis: {
-                name: xAxisName,
-                type: 'category',
-                boundaryGap: false,
-                data: xData,
-                splitNumber: 10
-            },
-            yAxis: {
-                name: yAxisName,
-                type: 'value',
-                max: Math.ceil(Math.max.apply(null, yData)) + 5,
-                min: Math.ceil(Math.min.apply(null, yData)) - 5,
-                data: yData
-            },
-            series: [{
-                data: yData,
-                name: yAxisName,
-                type: 'line',
-                smooth: true,
-                symbol: 'none',
-                areaStyle: {
-                    normal: {
-                        color: new echarts.graphic.LinearGradient(0, 1, 0, 0,
-                            [{ offset: 0, color: '#FFFFFF' },
-                            { offset: 1, color: '#06C46D' },
-                            ])
+
+    var docChart = document.getElementById("main")
+    docChart.style.width = (document.body.clientWidth)
+    docChart.style.height = '400px'
+    var myChart = echarts.init(docChart);
+
+    function DrawWeight(yData, xData, xAxisName, yAxisName) {
+        var option = {
+            baseOption: {
+                xAxis: {
+                    name: xAxisName,
+                    type: 'category',
+                    boundaryGap: false,
+                    data: xData,
+                    splitNumber: 10
+                },
+                yAxis: {
+                    name: yAxisName,
+                    type: 'value',
+                    max: Math.ceil(Math.max.apply(null, yData)) + 5,
+                    min: Math.ceil(Math.min.apply(null, yData)) - 5,
+                    data: yData
+                },
+                series: [{
+                    data: yData,
+                    name: yAxisName,
+                    type: 'line',
+                    smooth: true,
+                    symbol: 'none',
+                    areaStyle: {
+                        normal: {
+                            color: new echarts.graphic.LinearGradient(0, 1, 0, 0,
+                                [{ offset: 0, color: '#FFFFFF' },
+                                { offset: 1, color: '#06C46D' },
+                                ])
+                        }
+                    },
+                    lineStyle: {
+                        color: '#06C46D'
+                    }
+                }],
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        animation: false,
+                        label: {
+                            backgroundColor: '#505765'
+                        }
                     }
                 },
-                lineStyle: {
-                    color: '#06C46D'
+                grid: {
+                    show: true
                 }
-            }],
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'cross',
-                    animation: false,
-                    label: {
-                        backgroundColor: '#505765'
+            },
+            /*
+            media: [
+                {
+                    option: {
+                        legend: {
+                            orient: 'horizontal',
+                            left: 'right',
+                            itemGap: 10
+                        },
+                        xAxis: {
+                            nameLocation: 'end',
+                            nameGap: 10,
+                            splitNumber: 5,
+                        },
+                        series: [
+                            { name: xAxisName, center: ['75%', '20%'], radius: '28%' },
+                            { name: yAxisName, center: ['75%', '20%'], radius: '28%' }
+                        ]
+                    }
+                },
+                {
+                    query: { maxWidth: 670, minWidth: 550 },
+                    option: {
+                        legend: {
+                            orient: 'horizontal',
+                            left: 200,
+                            itemGap: 5,
+                        },
+                        grid: {
+                            left: '10%',
+                            top: 80,
+                            right: 90,
+                            bottom: 100
+                        },
+                        xAxis: {
+                            nameLocation: 'end',
+                            nameGap: 10,
+                            splitNumber: 5,
+                        },
+                        series: [
+                            { name: xAxisName, center: ['75%', '20%'], radius: '28%' },
+    
+                        ]
+                    }
+                },
+                {
+                    query: { maxWidth: 550 },
+                    option: {
+                        legend: {
+                            orient: 'vertical',
+                            left: 'right',
+                            itemGap: 5
+                        },
+                        grid: {
+                            left: 55,
+                            top: '32%',
+                            right: 100,
+                            bottom: 50
+                        },
+                        xAxis: {
+                            nameLocation: 'middle',
+                            nameGap: 25,
+                            splitNumber: 3
+                        },
+                        series: [
+                            { name: xAxisName, center: ['45%', '20%'], radius: '28%' },
+                            { name: yAxisName, center: ['45%', '20%'], radius: '28%' }
+                        ]
                     }
                 }
-            },
-            grid: {
-                show: true
-            }
-        },
-        /*
-        media: [
-            {
-                option: {
-                    legend: {
-                        orient: 'horizontal',
-                        left: 'right',
-                        itemGap: 10
-                    },
-                    xAxis: {
-                        nameLocation: 'end',
-                        nameGap: 10,
-                        splitNumber: 5,
-                    },
-                    series: [
-                        { name: xAxisName, center: ['75%', '20%'], radius: '28%' },
-                        { name: yAxisName, center: ['75%', '20%'], radius: '28%' }
-                    ]
-                }
-            },
-            {
-                query: { maxWidth: 670, minWidth: 550 },
-                option: {
-                    legend: {
-                        orient: 'horizontal',
-                        left: 200,
-                        itemGap: 5,
-                    },
-                    grid: {
-                        left: '10%',
-                        top: 80,
-                        right: 90,
-                        bottom: 100
-                    },
-                    xAxis: {
-                        nameLocation: 'end',
-                        nameGap: 10,
-                        splitNumber: 5,
-                    },
-                    series: [
-                        { name: xAxisName, center: ['75%', '20%'], radius: '28%' },
-
-                    ]
-                }
-            },
-            {
-                query: { maxWidth: 550 },
-                option: {
-                    legend: {
-                        orient: 'vertical',
-                        left: 'right',
-                        itemGap: 5
-                    },
-                    grid: {
-                        left: 55,
-                        top: '32%',
-                        right: 100,
-                        bottom: 50
-                    },
-                    xAxis: {
-                        nameLocation: 'middle',
-                        nameGap: 25,
-                        splitNumber: 3
-                    },
-                    series: [
-                        { name: xAxisName, center: ['45%', '20%'], radius: '28%' },
-                        { name: yAxisName, center: ['45%', '20%'], radius: '28%' }
-                    ]
-                }
-            }
-        ],
-        //options: {}
-        */
-    };
-
-    return option
-}
-
-
-
-//重量
-function ChartWeight() {
-
-    var ydata = [];
-    var xdata = [];
-    for (var i = 1; i < 31; i++) {
-        ydata.push(50 + Math.random());
-        xdata.push("2018-08-" + i);
-
-    }
-    
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "体重(kg)"));
-    setTimeout(function (){
-        window.onresize = function () {
-            myChart.resize();
-        }
-    });
-
-}
-//脂肪率
-function ChartFat() {
-
-    var ydata = [];
-    var xdata = [];
-    for (var i = 1; i < 31; i++) {
-        ydata.push(30 + Math.random());
-        xdata.push("2018-08-" + i);
-
-    }
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "脂肪率(%)"));
-    setTimeout(function (){
-        window.onresize = function () {
-            myChart.resize();
+            ],
+            //options: {}
+            */
         };
-    });
-}
 
-//肌肉量
-function ChartMusle() {
-
-    var ydata = [];
-    var xdata = [];
-    for (var i = 1; i < 31; i++) {
-        ydata.push(10 + Math.random());
-        xdata.push("2018-08-" + i);
-
+        return option
     }
 
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "脂肪量"));
-    setTimeout(function (){
-        window.onresize = function () {
-            myChart.resize();
+
+
+    //重量
+    function ChartWeight() {
+        //var width = docChart.style.width = $("chart_box").css("style", "width")
+        //var height = docChart.style.height = $("chart_box").css("style", "height")
+        alert(width);
+        alert(height);
+        var ydata = [];
+        var xdata = [];
+        for (var i = 1; i < 31; i++) {
+            ydata.push(50 + Math.random());
+            xdata.push("2018-08-" + i);
+
         }
-    });
-}
 
-//水分率
-function ChartWater() {
-
-
-    var ydata = [];
-    var xdata = [];
-    for (var i = 1; i < 31; i++) {
-        ydata.push(30 + Math.random());
-        xdata.push("2018-08-" + i);
-
+        myChart.setOption(DrawWeight(ydata, xdata, "日期", "体重(kg)"));
     }
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "水分率"));
-    setTimeout(function (){
-        window.onresize = function () {
-            myChart.resize();
+    //脂肪率
+    function ChartFat() {
+
+        var ydata = [];
+        var xdata = [];
+        for (var i = 1; i < 31; i++) {
+            ydata.push(30 + Math.random());
+            xdata.push("2018-08-" + i);
+
         }
-    });
-}
-
-
-//骨量
-function ChartBound() {
-
-    var ydata = [];
-    var xdata = [];
-    for (var i = 1; i < 31; i++) {
-        ydata.push(10 + Math.random());
-        xdata.push("2018-08-" + i);
-
+        myChart.setOption(DrawWeight(ydata, xdata, "日期", "脂肪率(%)"));
     }
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "骨量"));
-    setTimeout(function (){
-        window.onresize = function () {
-            myChart.resize();
+
+    //肌肉量
+    function ChartMusle() {
+
+        var ydata = [];
+        var xdata = [];
+        for (var i = 1; i < 31; i++) {
+            ydata.push(10 + Math.random());
+            xdata.push("2018-08-" + i);
+
         }
-    });
+
+        myChart.setOption(DrawWeight(ydata, xdata, "日期", "脂肪量"));
+    }
+
+    //水分率
+    function ChartWater() {
 
 
-}
+        var ydata = [];
+        var xdata = [];
+        for (var i = 1; i < 31; i++) {
+            ydata.push(30 + Math.random());
+            xdata.push("2018-08-" + i);
+
+        }
+        myChart.setOption(DrawWeight(ydata, xdata, "日期", "水分率"));
+    }
 
 
-document.getElementById("li_1").addEventListener("click", ChartWeight, false)
-document.getElementById("li_2").addEventListener("click", ChartFat, false)
-document.getElementById("li_3").addEventListener("click", ChartMusle, false)
-document.getElementById("li_4").addEventListener("click", ChartWater, false)
-document.getElementById("li_5").addEventListener("click", ChartBound, false)
-window.addEventListener("resize", function () {
-    myChart.resize();
-});
+    //骨量
+    function ChartBound() {
+
+        var ydata = [];
+        var xdata = [];
+        for (var i = 1; i < 31; i++) {
+            ydata.push(10 + Math.random());
+            xdata.push("2018-08-" + i);
+
+        }
+        myChart.setOption(DrawWeight(ydata, xdata, "日期", "骨量"));
+    }
+
+
+    document.getElementById("li_1").addEventListener("click", ChartWeight, false)
+    document.getElementById("li_2").addEventListener("click", ChartFat, false)
+    document.getElementById("li_3").addEventListener("click", ChartMusle, false)
+    document.getElementById("li_4").addEventListener("click", ChartWater, false)
+    document.getElementById("li_5").addEventListener("click", ChartBound, false)
+
+
