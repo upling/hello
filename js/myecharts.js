e6f2461bc77c61b47ba3a3328a782161e88ad5e0
lip <lip@hojy.com>
2018-08-27 15:42:40 +0800

    小数点截取

--- js/myecharts.js
+++ js/myecharts.js
@@ -5,11 +5,10 @@ docChart.style.width = (document.body.clientWidth)
 docChart.style.height = '300px'
 var myChart = echarts.init(docChart);
 
-function DrawWeight(yData, xData, xAxisName, yAxisName) {
+function DrawWeight(yData, xData, yAxisName) {
     var option = {
         baseOption: {
             xAxis: {
-                name: xAxisName,
                 type: 'category',
                 boundaryGap: false,
                 data: xData,
@@ -54,79 +53,7 @@ function DrawWeight(yData, xData, xAxisName, yAxisName) {
                 show: true
             }
         },
-        /*
-        media: [
-            {
-                option: {
-                    legend: {
-                        orient: 'horizontal',
-                        left: 'right',
-                        itemGap: 10
-                    },
-                    xAxis: {
-                        nameLocation: 'end',
-                        nameGap: 10,
-                        splitNumber: 5,
-                    },
-                    series: [
-                        { name: xAxisName, center: ['75%', '20%'], radius: '28%' },
-                        { name: yAxisName, center: ['75%', '20%'], radius: '28%' }
-                    ]
-                }
-            },
-            {
-                query: { maxWidth: 670, minWidth: 550 },
-                option: {
-                    legend: {
-                        orient: 'horizontal',
-                        left: 200,
-                        itemGap: 5,
-                    },
-                    grid: {
-                        left: '10%',
-                        top: 80,
-                        right: 90,
-                        bottom: 100
-                    },
-                    xAxis: {
-                        nameLocation: 'end',
-                        nameGap: 10,
-                        splitNumber: 5,
-                    },
-                    series: [
-                        { name: xAxisName, center: ['75%', '20%'], radius: '28%' },
- 
-                    ]
-                }
-            },
-            {
-                query: { maxWidth: 550 },
-                option: {
-                    legend: {
-                        orient: 'vertical',
-                        left: 'right',
-                        itemGap: 5
-                    },
-                    grid: {
-                        left: 55,
-                        top: '32%',
-                        right: 100,
-                        bottom: 50
-                    },
-                    xAxis: {
-                        nameLocation: 'middle',
-                        nameGap: 25,
-                        splitNumber: 3
-                    },
-                    series: [
-                        { name: xAxisName, center: ['45%', '20%'], radius: '28%' },
-                        { name: yAxisName, center: ['45%', '20%'], radius: '28%' }
-                    ]
-                }
-            }
-        ],
-        //options: {}
-        */
+        
     };
 
     return option
@@ -142,7 +69,7 @@ function ChartWeight() {
         ydata.push(50 + Math.random());
         xdata.push("2018-08-" + i);
     }
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "体重(kg)"));
+    myChart.setOption(DrawWeight(ydata, xdata, "体重(kg)"));
 }
 //脂肪率
 function ChartFat() {
@@ -150,11 +77,11 @@ function ChartFat() {
     var ydata = [];
     var xdata = [];
     for (var i = 1; i < 31; i++) {
-        ydata.push(30 + Math.random());
+        ydata.push(30 + Math.random().toFixed(2));
         xdata.push("2018-08-" + i);
 
     }
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "脂肪率(%)"));
+    myChart.setOption(DrawWeight(ydata, xdata, "脂肪率(%)"));
 }
 
 //肌肉量
@@ -163,12 +90,12 @@ function ChartMusle() {
     var ydata = [];
     var xdata = [];
     for (var i = 1; i < 31; i++) {
-        ydata.push(10 + Math.random());
+        ydata.push(10 + Math.random().toFixed(2));
         xdata.push("2018-08-" + i);
 
     }
 
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "脂肪量"));
+    myChart.setOption(DrawWeight(ydata, xdata, "脂肪量"));
 }
 
 //水分率
@@ -178,11 +105,11 @@ function ChartWater() {
     var ydata = [];
     var xdata = [];
     for (var i = 1; i < 31; i++) {
-        ydata.push(30 + Math.random());
+        ydata.push(30 + Math.random().toFixed(2));
         xdata.push("2018-08-" + i);
 
     }
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "水分率"));
+    myChart.setOption(DrawWeight(ydata, xdata, "水分率"));
 }
 
 
@@ -192,11 +119,11 @@ function ChartBound() {
     var ydata = [];
     var xdata = [];
     for (var i = 1; i < 31; i++) {
-        ydata.push(10 + Math.random());
+        ydata.push(10 + Math.random().toFixed(2));
         xdata.push("2018-08-" + i);
 
     }
-    myChart.setOption(DrawWeight(ydata, xdata, "日期", "骨量"));
+    myChart.setOption(DrawWeight(ydata, xdata, "骨量"));
 }
 
 
@@ -206,7 +133,7 @@ document.getElementById("li_3").addEventListener("click", ChartMusle, false)
 document.getElementById("li_4").addEventListener("click", ChartWater, false)
 document.getElementById("li_5").addEventListener("click", ChartBound, false)
 
-window.o
+ChartWeight();
 window.onresize = function () {
     docChart.style.width = (document.body.clientWidth)
     docChart.style.height = '300px'
